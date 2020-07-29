2be77a68f9bfec1d0f6d2fe4ed5a11150a0f72eb
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2020-07-29 09:59:54 -0500

    Update fibo2.py

--- fibo2.py
+++ fibo2.py
@@ -1,6 +1,6 @@
 fibo = [0,1]
 n = input("¿Cuantos términos Fibonacci quieres obtener? ")
 n = int(n)
-for i in range (n-2):
+for i in range (n):
     fibo.append(fibo[-1]+fibo[-2])
     print(fibo[i])
